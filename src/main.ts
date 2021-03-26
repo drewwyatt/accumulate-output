356a5fd60df53a5f903efb6828b86d6d75981340
Drew Wyatt <drew.j.wyatt@gmail.com>
2021-03-25 22:09:50 -0400

    misc cleanup + hello world

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core'
 
 const run = async () => {
   try {
-    // ...
+    core.setOutput('hello', 'world')
   } catch (err: unknown) {
     if (err instanceof Error) {
       core.setFailed(err.message)
