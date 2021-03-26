742b08b73c5af76d30492ac69a0ce48f7ba5921d
Drew Wyatt <drew.j.wyatt@gmail.com>
2021-03-25 22:38:11 -0400

    we got a little too fancy there

--- dist/index.js
+++ dist/index.js
@@ -26,11 +26,21 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(186));
-const run = async () => {
+const run = () => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
     try {
-        const outputs = core.getInput('outputs') ?? '[]';
+        const outputs = (_a = core.getInput('outputs')) !== null && _a !== void 0 ? _a : '[]';
         const key = core.getInput('key', { required: true });
         const value = core.getInput('value', { required: true });
         core.info(`outputs: ${outputs}`);
@@ -46,7 +56,7 @@ const run = async () => {
             core.setFailed(String(err));
         }
     }
-};
+});
 run();
 
 
